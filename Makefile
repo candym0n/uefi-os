5b9a94100ce2b3aebca5cb289b070adbcd5419ff
candym0n <jayden@bascio.com>
2025-03-04 19:19:01 +0800

    Fixed Makefile and directory structure

--- Makefile
+++ Makefile
@@ -1,24 +1,41 @@
 .PHONY: all clean run image bootloader tools
 
-all: image
+# Shush Makefile
+MAKEFLAGS += --silent
 
-run:
-	@sh scripts/qemu.sh
+SIZE := 1G
+
+TARGET := build/test.img
+
+GPTIMG_DIR := tools/gptimg
+
+BOOT_DIR := boot
+
+BUILD_SCRIPT := scripts/build.sh
+QEMU_SCRIPT := scripts/qemu.sh
+
+# Export all of the variables for scripts to use
+export SIZE TARGET GPTIMG_DIR BOOT_DIR
+
+all: image
 
 clean:
 	@echo "Cleaning up..."
-	@cd src/boot && make clean && cd ../..
-	@cd tools/gptimg && make clean && cd ../..
-	rm -f test.img
+	@cd $(BOOT_DIR) && make clean && cd ../..
+	@cd $(GPTIMG_DIR) && make olsclean && cd ../..
+	rm -rf build
 
 bootloader:
 	@echo "Building bootloader..."
-	@cd src/boot && make all && cd ../..
+	@cd $(BOOT_DIR) && make all && cd ../..
 
 tools:
 	@echo "Building tools..."
-	@cd tools/gptimg && make all && cd ../..
+	@cd $(GPTIMG_DIR) && make all && cd ../..
 
 image: bootloader tools
 	@echo "Creating image..."
-	@sh scripts/build.sh
+	@bash $(BUILD_SCRIPT)
+
+run:
+	@bash $(QEMU_SCRIPT)
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,24 @@
-IMAGE=test.img
-SIZE=1G
-alias GPTIMG="tools/gptimg/build/gptimg"
+#!bin/bash
+
+#TARGET=build/test.img
+#SIZE=1G
+GPTIMG="$GPTIMG_DIR/build/gptimg"
 
 # Create the image
 echo "Creating disk image..."
-GPTIMG create "$IMAGE" --size "$SIZE"
+$GPTIMG create "$TARGET" --size "$SIZE"
 
 # Add partitions
 echo "Adding partitions to disk image..."
-esp_partition=$(GPTIMG add-partition "$IMAGE" --size 64M --type efi --name "EFI System Partition")
-os_partition=$(GPTIMG add-partition "$IMAGE" --size 512M --type basic-data --name "Operating System")
-data_partition=$(GPTIMG add-partition "$IMAGE" --size 256M --type basic-data --name "Basic Data")
+esp_partition=$($GPTIMG add-partition "$TARGET" --size 64M --type efi --name "EFI System Partition")
+os_partition=$($GPTIMG add-partition "$TARGET" --size 512M --type basic-data --name "Operating System")
+data_partition=$($GPTIMG add-partition "$TARGET" --size 256M --type basic-data --name "Basic Data")
 
 # Setup the loopback device and mount file
 echo "Setting up loopback device..."
 loop_device=$(losetup -f)
 mount_point=$(mktemp -d)
-sudo losetup "$loop_device" "$IMAGE" -P
+sudo losetup "$loop_device" "$TARGET" -P
 
 # Format the partitions
 echo "Formatting partitions..."
@@ -24,7 +26,7 @@ sudo mkfs.fat -F 32 "$loop_device"p"$esp_partition" # ESP -> FAT32
 
 # Add EFI/BOOT/BOOTX64.EFI to the ESP (using a mount sandwich)
 echo "Adding bootloader to EFI System Partition..."
-BOOT_FILE=src/boot/build/BOOTX64.EFI
+BOOT_FILE=$BOOT_DIR/build/BOOTX64.EFI
 sudo mount "$loop_device"p"$esp_partition" "$mount_point"   # Top bun (mount)
 sudo mkdir -p $mount_point/EFI/BOOT                         # Meat (create dir)
 sudo cp $BOOT_FILE $mount_point/EFI/BOOT/BOOTX64.EFI        # Meat (copy file)
@@ -35,4 +37,4 @@ sudo losetup -d "$loop_device"
 sudo rm -r "$mount_point"
 
 # Change file permissions
-sudo chmod a+rw $IMAGE
+sudo chmod a+rw $TARGET
